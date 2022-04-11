6759eaf1f659fd98c4b54e74edb02f536d5ad6e5
adamerwin44 <99776356+adamerwin44@users.noreply.github.com>
2022-04-10 20:29:22 -0400

    Add files via upload

---
+++ project_06/arcade/options.js
@@ -0,0 +1,68 @@
+/*
+//this js controls the menu selections
+
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
+}
+//frank said have a counter, is this even or odd , which one
+//
+
+
+
+const canvas = document.getElementById("cvs");
+const ctx =canvas.getContext("2d");
+
+below are event listeners which are functions responding to user input such as a mouse click 
+
+computerBtn.addEventListener(click, function(){
+OPPONET ="computer";
+switchActive (friendBtn, computerBtn);
+});
+
+friendBtn.addEventListener(click, function ()
+{OPPONET = "friend";
+switchActive (computerBtn, friendBtn);                              
+});
+
+oBtn.addEventListener(click, function(){
+    player.man ="o";
+    player.computer="x";
+    player.friend="x";
+    switchActive (xBtn, oBtn);
+        });
+xBtn.addEventListener(click, function() {
+    player.man ="x";
+    player.computer="o";
+    player.friend="o";
+    switchActive (oBtn, xBtn);
+});
+startBtn.addEventListener(click, function(){
+    if ( !opponent ){
+
+
+    }
+    
+
+});
+
+function switchActive(off, on){
+off.classList.remove("active");
+on.classList.add("active");
+}
+
+function init(player, OPPONET){
+  const canvas = document.getElementById("cvs")
+  const ctx = canvas.getContext("2d");
+    const COLUMN = 3;
+    const ROW = 3;
+    let board =[];
+     const SPACE_SIZE = 150;
+
+for(let i = 0; i < ROW; i++){
+    board[i] = [];
+    for(let i = 0; i < COLUMN; i++)
+    board[i] = [];
+}
+}
+*/
